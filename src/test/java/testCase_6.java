2b3d7828017898db35bd6d21c5ae59dc2a89b7cf
rahilshaikh20 <rahilshaikh595@yahoo.com>
2023-10-08 00:14:24 +0530

    Added Passed TC

---
+++ src/test/java/testCase_6.java
@@ -0,0 +1,22 @@
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import utilities.ExtentReportSetUp;
+
+public class testCase_6 extends ExtentReportSetUp
+{
+
+    @Test
+    public void testCase5()
+ {
+     test=extent.createTest("TC_0006");
+     test.info("6th TC started");
+     test.fail("Test case passed");
+     System.out.println("*****TC 6 Passed****");
+
+ }
+ @AfterClass
+    public void tearDown()
+ {
+
+ }
+}
